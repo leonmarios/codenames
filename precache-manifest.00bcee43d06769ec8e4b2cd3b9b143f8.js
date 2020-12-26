33e7049012e9b75b20beb3cfcad4d470203d6e01
Marios Leon <leonmarios@gmail.com>
2020-12-26 20:23:58 +0200

    gh

---
+++ precache-manifest.00bcee43d06769ec8e4b2cd3b9b143f8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a308482d9b854692c90f4ae9b3efee6",
+    "url": "/codenames/index.html"
+  },
+  {
+    "revision": "1922508dfb6ac8b3280e",
+    "url": "/codenames/static/css/main.8ddff368.chunk.css"
+  },
+  {
+    "revision": "52273797abc85966b9a0",
+    "url": "/codenames/static/js/2.70672278.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codenames/static/js/2.70672278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1922508dfb6ac8b3280e",
+    "url": "/codenames/static/js/main.db4544c4.chunk.js"
+  },
+  {
+    "revision": "4caf5b2b468c6e8127f8",
+    "url": "/codenames/static/js/runtime-main.cb01a233.js"
+  },
+  {
+    "revision": "90595896067c8dca8953210ba731be3e",
+    "url": "/codenames/static/media/greece.90595896.svg"
+  },
+  {
+    "revision": "9d579f759224e6fec1a7fdc0fa56701e",
+    "url": "/codenames/static/media/main.9d579f75.png"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/codenames/static/media/uk.e5564902.svg"
+  }
+]);
